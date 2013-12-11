795db7c002b6a7f72e23d47bdd0b97fe0ebb8528
Adrian <adrienfrancis.bourgeois@gmail.com>
2013-12-11 16:28:57 +1100

    finding the exact name with the extension of the manifest files

--- lib/generators/install_jqtouch_generator.rb
+++ lib/generators/install_jqtouch_generator.rb
@@ -1,11 +1,20 @@
+require 'find'
+
 class InstallJqtouchGenerator < Rails::Generators::Base
 
   source_root File.expand_path("../lib/jqtouch", __FILE__)
 
   def create_initializer_file
     create_file "app/assets/javascripts/jqtouch.js", "var jqtouch = $.jQTouch({});"
-    insert_into_file "app/assets/javascripts/application.js", "//= require_tree ../../../vendor/assets/javascripts/jqtouch\n", after: "//= require jquery_ujs\n"
-    insert_into_file "app/assets/stylesheets/application.css", " *= require ../../../vendor/assets/stylesheets/jqtouch/css/apple\n", after: "*= require_self\n"
+
+    css_manifest_path = nil
+    Find.find('app/assets/stylesheets/') { |path| css_manifest_path = path if path =~ /application/ }
+
+    js_manifest_path = nil
+    Find.find('app/assets/javascripts/') { |path| js_manifest_path = path if path =~ /application/ }
+
+    insert_into_file js_manifest_path, "//= require_tree ../../../vendor/assets/javascripts/jqtouch\n", after: "//= require jquery_ujs\n" if js_manifest_path
+    insert_into_file css_manifest_path, " *= require ../../../vendor/assets/stylesheets/jqtouch/css/apple\n", after: "*= require_self\n" if css_manifest_path
     copy_file "javascripts/jqtouch-jquery.min.js", "vendor/assets/javascripts/jqtouch/jqtouch-jquery.min.js"
     copy_file "javascripts/jqtouch.min.js", "vendor/assets/javascripts/jqtouch/jqtouch.min.js"
     copy_file "stylesheets/apple.css", "vendor/assets/stylesheets/jqtouch/css/apple.css"
